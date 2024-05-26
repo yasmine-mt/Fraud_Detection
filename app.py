504415c07280a924b9f375d66b6b3f6b40c9132e
Yasmine Maataoui <yasminemaataoui12@gmail.com>
2024-05-26 23:53:19 +0100

    Prediction

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import pandas as pd
+import joblib
 app = Flask(__name__)
 
+model = joblib.load('rf_model.pkl')
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -10,15 +12,32 @@ def home():
 def data():
     return render_template('data.html')
 
-@app.route('/predict')
-def predict():
-    return render_template('predict.html')
 
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    prediction_result = None
+    if request.method == 'POST':
+        # Get form data
+        oldbalanceOrg = float(request.form.get('oldbalanceOrg'))
+        newbalanceOrig = float(request.form.get('newbalanceOrig'))
+        oldbalanceDest = float(request.form.get('oldbalanceDest'))
+        newbalanceDest = float(request.form.get('newbalanceDest'))
+
+        # Perform prediction using the loaded model
+        prediction = model.predict([[ oldbalanceOrg, newbalanceOrig, oldbalanceDest, newbalanceDest]])
+
+        # Interpret prediction result
+        if prediction[0] == 1:
+            prediction_result = "Fraudulent transaction"
+        else:
+            prediction_result = "Non-fraudulent transaction"
+
+    return render_template('predict.html', prediction_result=prediction_result)
 
 @app.route('/performance')
 def performance():
     # Charger les données du fichier CSV
-    metrics_df = pd.read_csv('model_metrics_hyper.csv')
+    metrics_df = pd.read_csv('model_metrics.csv')
 
     # Convertir les données en dictionnaire pour passer à la page HTML
     model_metrics = {}
